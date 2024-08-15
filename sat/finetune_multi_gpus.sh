3917928abaf36ec12827cd9377bfbfda0c5eda6f
zR <2448370773@qq.com>
2024-08-15 22:48:24 +0800

    update reference

--- sat/finetune_multi_gpus.sh
+++ sat/finetune_multi_gpus.sh
@@ -2,7 +2,7 @@
 
 echo "RUN on `hostname`, CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 
-run_cmd="torchrun --standalone --nproc_per_node=4 train_video.py --base configs/cogvideox_2b_sft.yaml --seed $RANDOM“
+run_cmd="torchrun --standalone --nproc_per_node=4 train_video.py --base configs/cogvideox_2b_sft.yaml --seed $RANDOM"
 
 echo ${run_cmd}
 eval ${run_cmd}
