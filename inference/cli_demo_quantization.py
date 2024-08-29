f9a49ad88f5f69081de54e34185016949fd008be
zR <2448370773@qq.com>
2024-08-29 18:13:00 +0800

    Further optimize memory usage.

--- inference/cli_demo_quantization.py
+++ inference/cli_demo_quantization.py
@@ -85,6 +85,8 @@ def generate_video(
     # pipe.transformer.to(memory_format=torch.channels_last)
     # for FP8 should remove  pipe.enable_model_cpu_offload()
     pipe.enable_model_cpu_offload()
+    pipe.enable_sequential_cpu_offload()
+    pipe.vae.enable_slicing()
     pipe.vae.enable_tiling()
     video = pipe(
         prompt=prompt,
